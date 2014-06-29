32876c9a4665fcfd8520485661bdba927c44c608
Christopher Baker <me@christopherbaker.net>
2014-06-29 14:53:50 -0500

    Added addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxPugiXML
+	ADDON_DESCRIPTION = A thin wrapper for pugixml http://pugixml.org.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "xml" "xpath"
+	ADDON_URL = http://github.com/bakercp/ofxPugiXML
